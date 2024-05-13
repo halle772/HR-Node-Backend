7d667f5e7638b688ad2b83a0994cda265b703426
ns2772 <bytelogic772@gmail.com>
2024-05-13 11:17:13 +0500

    [FIX]: Protected Data

--- db/database.js
+++ db/database.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const dbURI = "mongodb+srv://alihasaninbox:glm3yYMpiWQoCEAE@hrapp.rstg7aa.mongodb.net/?retryWrites=true&w=majority&appName=hrapp";
+const dbURI = `mongodb+srv://${process.env.DB_HOST_USER}:${process.env.DB_HOST_PASS}@hrapp.rstg7aa.mongodb.net/?retryWrites=true&w=majority&appName=hrapp`;
 
 mongoose.connect(dbURI, {
     useUnifiedTopology: true,
